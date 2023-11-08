d29891c6c17008aa85b7d50f045f773a92302d6c
devakashsharma <soulakash20002@gmail.com>
2023-11-09 00:12:28 +0530

    yolo set-up

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# At first we need to import some module
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Now we will set up yolo v3 and it's weight, cfg and we will also add coco name file which is objects names
+yolo = cv2.dnn.readNet("C:/Users/soula/Downloads/yolov3-tiny.weights", "C:/Users/soula/Downloads/yolov3-tiny.cfg")
+classes = []
+with open("C:/Users/soula/Downloads/coco.names") as f:
+  classes = f.read().splitlines()
+
